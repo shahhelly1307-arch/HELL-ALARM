c4cf10f4de8a559f0630eb86a8b96403d4f586f3
shahhelly1307-arch <shahhelly1307@gmail.com>
2026-06-10 05:56:06 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   root: 'src/renderer',
+  publicDir: resolve(__dirname, 'assets'),
   build: {
     outDir: '../../dist'
   }
