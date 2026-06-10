26412cc97969bbdeb5daa00dc6347f40fa6000b9
shahhelly1307-arch <shahhelly1307@gmail.com>
2026-06-10 06:10:13 -0700

    Update env.d.ts

--- src/renderer/src/env.d.ts
+++ src/renderer/src/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+/// <reference types="vite/client" />
+
+interface Window {
+  api?: {
+    notify: (title: string, body: string, silent?: boolean) => Promise<boolean>
+  }
+}
